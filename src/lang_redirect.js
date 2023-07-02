5854f78c811e9ddbce59fa4f09f77f424c9bba4b
Widrolo <29biernat.filip04@gmail.com>
2023-07-02 02:00:46 +0200

    Added another project and polish translation

--- src/lang_redirect.js
+++ src/lang_redirect.js
@@ -4,6 +4,10 @@ if (userlang == "de" || userlang == "de-AT")
 {
     window.location.href = 'de/home.html';
 }
+else if (userlang == "pl")
+{
+    window.location.href = 'pl/home.html';
+}
 else
 {
     window.location.href = 'en/home.html';
